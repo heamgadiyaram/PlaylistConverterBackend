59c88119c2456124c5fa613dfd44f2404db18fcc
Heam Gadiyaram <heam.gadiyaram07@gmail.com>
2025-03-14 01:14:47 -0400

    changed selenium chrome options

--- AppleMusicWebScrape.py
+++ AppleMusicWebScrape.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 def web_scrape(link):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
     url = link
     driver.get(url)
